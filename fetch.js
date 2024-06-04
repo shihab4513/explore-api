7f6635548cbfb8f5907a26fb3893867a4c4450ed
shihab4513 <shihab4513@gmail.com>
2024-06-04 21:26:49 +0600

    app.js added index.html further edited. we did fetching in app.js but in different link. . Its a part of previous commit

--- fetch.js
+++ fetch.js
@@ -3,8 +3,8 @@
 function loadData() {
     const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-    fetch(url)
-        .then(Response => Response.json())
-        .then(Response => console.log(Response))
+    fetch(url)//get the data from url
+        .then(Response => Response.json()) //convert the data to javaScript object
+        .then(Response => console.log(Response))  //Display the javaScript object
 
 }
