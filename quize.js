c102d70c175a0008ca30aed4de27fbcc16af20d4
shihab4513 <shihab4513@gmail.com>
2024-06-05 23:18:13 +0600

    quize.js added . Here a quize question added + interview question png added

---
+++ quize.js
@@ -0,0 +1,3 @@
+const stuInfo = JSON.stringify({ name: "James", roll: 3 });
+// you have already converted object into string so with below code you cant access object property now
+console.log(stuInfo.name);
